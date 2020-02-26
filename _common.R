4b9893f9c115a53ae2d8f581107affb598abb1fe
C Lente <7017340+clente@users.noreply.github.com>
2020-02-26 00:50:34 -0300

    Common options

---
+++ _common.R
@@ -0,0 +1,15 @@
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  echo = FALSE,
+  dpi = 50,
+  collapse = TRUE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
